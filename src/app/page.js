26977ff270b3c2525aa696b14b071b9d26d4b731
TroyMoses <mugabimoses07@gmail.com>
2024-05-21 09:34:05 +0800

    typos

--- src/app/page.js
+++ src/app/page.js
@@ -16,10 +16,10 @@ export default function Home() {
         />
         <div className="text-gray-500 max-w-md text-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-          Founded in 2019, Emmaz Electronics was born from a passion for putting the power of technology in everyone's hands. We believe electronics should enhance your life, not complicate it. That's why we focus on offering a user-friendly online experience with a curated selection of top brands and the latest innovations.
+          Founded in 2019, Emmaz Electronics was born from a passion for putting the power of technology in the hands of every one. We believe electronics should enhance your life, not complicate it. That is why we focus on offering a user-friendly online experience with a curated selection of top brands and the latest innovations.
           </p>
-          <p>Our team of tech enthusiasts is here to guide you every step of the way. Whether you're a seasoned gadget guru or just starting your tech journey, we offer detailed product descriptions, helpful buying guides, and responsive customer support. We want you to feel confident and excited about your purchase!</p>
-          <p>Looking beyond the sale, we strive to be a valuable resource. We keep our blog updated with the latest tech trends, buying tips, and even troubleshooting guides. We're committed to building a community of tech-savvy individuals who can learn from each other and get the most out of their electronics.</p>
+          <p>Our team of tech enthusiasts is here to guide you every step of the way. Whether you are a seasoned gadget guru or just starting your tech journey, we offer detailed product descriptions, helpful buying guides, and responsive customer support. We want you to feel confident and excited about your purchase!</p>
+          <p>Looking beyond the sale, we strive to be a valuable resource. We keep our blog updated with the latest tech trends, buying tips, and even troubleshooting guides. We are committed to building a community of tech-savvy individuals who can learn from each other and get the most out of their electronics.</p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
